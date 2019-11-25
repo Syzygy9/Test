7d3ae386fa6461e1b1511bad6e87782784d16147
Syzygy9 <shakti_evgenija@mail.ru>
2019-11-25 23:56:10 +0300

    teenSum

---
+++ src/main/java/TeenSumMain.java
@@ -0,0 +1,8 @@
+public class TeenSumMain {
+
+    public static void main(String[] args) {
+        TeenSum teenSum = new TeenSum();
+        teenSum.teenSum(13,19);
+
+    }
+}
---
+++ src/test/java/TeenSumTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TeenSumTest {
+
+    @Test
+    public void teenSum() {
+        TeenSum teenSum = new TeenSum();
+        int actual = teenSum.teenSum(13,19);
+        int expected = 19;
+        assertEquals(actual, expected);
+            }
+
+    @Test
+    public void teenSum1() {
+        TeenSum teenSum1 = new TeenSum();
+        int actual = teenSum1.teenSum(12, 19);
+        int expected = 12 + 19;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void teenSum2() {
+        TeenSum teenSum2 = new TeenSum();
+        int actual = teenSum2.teenSum(13, 20);
+        int expected = 13 + 20;
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void teenSum3() {
+        TeenSum teenSum3 = new TeenSum();
+        int actual = teenSum3.teenSum(11, 21);
+        int expected = 11 + 21;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void teenSum4() {
+        TeenSum teenSum4 = new TeenSum();
+        int actual = teenSum4.teenSum(15, 16);
+        int expected = 19;
+        assertEquals(actual, expected);
+
+    }
+
+
+}
